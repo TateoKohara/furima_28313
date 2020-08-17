f1617c894d8212d28426fcac8724592270156687
TateoKohara <68531009+TateoKohara@users.noreply.github.com>
2020-08-17 12:25:04 +0900

    出品ページへの制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  # before_action :require_login
+  before_action :authenticate_user!, except: [:index]
+
 
   def index
   end  
@@ -8,18 +11,27 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
-    if @item.save
-      redirect_to root_path
-    else 
-      render :new
-    end    
+    # @user = 
+    # @item = Item.new
+    # if @item.save
+    #   redirect_to root_path
+    # else 
+    #   render :new
+    # end    
   end
 
-  def checked
-  end
+  # def checked
+  # end
 
   private
+
+  # def require_login
+  #   if user_signed_in?
+  #     render :items
+  #   else  
+  #     redirect_to new_user_session_path
+  #   end
+  # end
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :genre_id)
