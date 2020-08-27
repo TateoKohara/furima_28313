ddb2ccd72c46a04c9bad468d41120980c6d68ca2
TateoKohara <68531009+TateoKohara@users.noreply.github.com>
2020-08-27 16:39:12 +0900

    issue修正2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password,:family_name,:first_name,:family_name_j,:first_name_j,:birth])
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end  
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
 end
 
-def new
-  @user = User.new
-end
-  
-def create
-  User.create(user_params)
-end  
+  def new
+    @user = User.new
+  end
+    
+  def create
+    User.create(user_params)
+  end  
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(:nickname,:email,:password,:family_name,:first_name,:family_name_j,:first_name_j,:birth).merge(user_id: current_user.id)
-end
+  def user_params
+    params.require(:user).permit(:nickname,:email,:password,:family_name,:first_name,:family_name_j,:first_name_j,:birth).merge(user_id: current_user.id)
+  end
+
+
+
